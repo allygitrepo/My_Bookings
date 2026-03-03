2db31d08e8331157d37c157f526c3d7e2d93e9a7
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-03-03 11:02:45 +0530

    route index file

---
+++ server/routes/routes.index.js
@@ -0,0 +1,29 @@
+const userRoutes = require("./user.routes");
+const businessRoutes = require("./business.routes");
+const locationRoutes = require("./location.routes");
+const staffRoutes = require("./staff.routes");
+const serviceRoutes = require("./service.routes");
+const staffServiceRoutes = require("./staffService.routes");
+const staffAvailabilityRoutes = require("./staffAvailability.routes");
+const customerRoutes = require("./customer.routes");
+const bookingRoutes = require("./booking.routes");
+const paymentRoutes = require("./payment.routes");
+const apiKeyRoutes = require("./apiKey.routes");
+
+const routes = (app) => {
+    const prefix = "/mybookings";
+
+    app.use(`${prefix}/users`, userRoutes);
+    app.use(`${prefix}/business`, businessRoutes);
+    app.use(`${prefix}/locations`, locationRoutes);
+    app.use(`${prefix}/staff`, staffRoutes);
+    app.use(`${prefix}/services`, serviceRoutes);
+    app.use(`${prefix}/staff-services`, staffServiceRoutes);
+    app.use(`${prefix}/staff-availability`, staffAvailabilityRoutes);
+    app.use(`${prefix}/customers`, customerRoutes);
+    app.use(`${prefix}/bookings`, bookingRoutes);
+    app.use(`${prefix}/payments`, paymentRoutes);
+    app.use(`${prefix}/api-keys`, apiKeyRoutes);
+};
+
+module.exports = routes;
